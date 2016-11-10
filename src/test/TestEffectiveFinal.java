2e9c2ad9b2595bdcb8e53be4e9999f884e4e278c
shashank2apr <shashank2apr@gmail.com>
2016-11-11 00:12:21 +0530

    got rid of useless code

--- src/test/TestEffectiveFinal.java
+++ src/test/TestEffectiveFinal.java
@@ -1,36 +1,20 @@
 package test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class TestEffectiveFinal {
-	
-	public static void main(String[] args) {
-		
-		final String outer="Outer";
-	/*	
-		Future future = pool.submit(new Callable<Future>() {
 
-			@Override
-			public Future call() throws Exception {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		});
-			*/
-		
+	public static void main(String[] args) {
+
+		final String outer = "Outer";
+
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				
+
 				System.out.println(outer);
 			}
 		}).start();
-		
-		
+
 	}
-	
+
 }
